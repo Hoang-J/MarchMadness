48ae1dbb3d500f8bba36494f7a37f3155e65f933
COR Coding <corcoding@NwugochisiPhone.localdomain>
2024-03-20 16:04:56 -0400

    adding initial in-progess branch set up for teams bracket

---
+++ ncaa.js
@@ -0,0 +1,483 @@
+
+// NCAA API 
+
+let ncaaSouth = [
+    {
+        teamName: "Alabama",
+        seedNumber: 1,
+        finalScore: 96,
+    },
+    {
+        teamName: "Texas A&M-Corpus Christi",
+        seedNumber: 16,
+        finalScore: 75,
+    },
+    {
+        teamName: "Maryland",
+        seedNumber: 8,
+        finalScore: 67,
+    },
+    {
+        teamName: "West Virgina",
+        seedNumber: 9,
+        finalScore: 65,
+    },
+    {
+        teamName: "San Diego St.",
+        seedNumber: 5,
+        finalScore: 63,
+         
+    },
+    {
+        teamName: "Col. of Charleston",
+        seedNumber: 12,
+        finalScore: 57,
+         
+    },
+    {
+        teamName: "Virginia",
+        seedNumber: 4,
+        finalScore: 67,
+         
+    },
+    {
+        teamName: "Furman",
+        seedNumber: 13,
+        finalScore: 68,
+         
+    },
+    {
+        teamName: "Creighton",
+        seedNumber: 6,
+        finalScore: 72,
+         
+    },
+    {
+        teamName: "NC State",
+        seedNumber: 11,
+        finalScore: 63,
+         
+    },
+    {
+        teamName: "Baylor",
+        seedNumber: 3,
+        finalScore: 74,
+         
+    },
+    {
+        teamName: "UC Santa Barbara",
+        seedNumber: 14,
+        finalScore: 56,
+         
+    },
+    {
+        teamName: "Missouri",
+        seedNumber: 7,
+        finalScore: 76,
+         
+    },
+    {
+        teamName: "Utah St.",
+        seedNumber: 10,
+        finalScore: 65,
+         
+    },
+    {
+        teamName: "Arizona",
+        seedNumber: 2,
+        finalScore: 55,
+         
+    },
+    {
+        teamName: "Princeton",
+        seedNumber: 15,
+        finalScore: 59,
+         
+    },
+
+];
+
+let ncaaMidwest = [
+    {
+        teamName: "Houston",
+        seedNumber: 1,
+        finalScore: 63,
+        
+    },
+    
+    {
+        teamName: "Northern Kentucky",
+        seedNumber: 16,
+        finalScore: 52,
+        
+    },
+    
+    {
+        teamName: "Iowa",
+        seedNumber: 8,
+        finalScore: 75,
+        
+    },
+    {
+        teamName: "Auburn",
+        seedNumber: 9,
+        finalScore: 83,
+        
+    },
+    
+    {
+        teamName: "Miami",
+        seedNumber: 5,
+        finalScore: 63,
+        
+    },
+    
+    {
+        teamName: "Drake",
+        seedNumber: 12,
+        finalScore: 56,
+        
+    },
+    
+    {
+        teamName: "Indiana",
+        seedNumber: 4,
+        finalScore: 71,
+        
+    },
+    {
+        teamName: "Kent St.",
+        seedNumber: 13,
+        finalScore: 60,
+        
+    },
+    {
+        teamName: "Iowa St",
+        seedNumber: 6,
+        finalScore: 52,
+        
+    },
+    
+    {
+        teamName: "Pittsburgh",
+        seedNumber: 11,
+        finalScore: 59,
+        
+    },
+    {
+        teamName: "Xavier",
+        seedNumber: 3,
+        finalScore: 72,
+        
+    },
+    {
+        teamName: "Kennesaw St",
+        seedNumber: 14,
+        finalScore: 67,
+    },
+
+    {
+        teamName: "Texas A&M",
+        seedNumber: 7,
+        finalScore: 59,
+        
+
+    },
+    {
+        teamName: "Penn St",
+        seedNumber: 10,
+        finalScore: 76,
+            
+    },
+    {
+        teamName: "Texas",
+        seedNumber: 2,
+        finalScore: 81,
+                
+    },
+              
+    {
+        teamName: "Colgate",
+        seedNumber: 15,
+        finalScore: 61,
+                    
+    },
+                  
+];
+
+let ncaaWest = [
+    {
+        teamName: "Kansas",
+        seedNumber: 1,
+        finalScore: 96,
+         
+    },
+    
+    {
+       teamName: "Howard",
+       seedNumber: 16,
+       finalScore: 68,
+        
+    },
+    {
+        teamName:"Arkansas",
+        seedNumber: 8,
+        finalScore: 73,
+         
+    },
+    {
+        teamName: "Illinois",
+        seedNumber: 9,
+        finalScore: 63,
+         
+    },
+    {
+        teamName:"Saint Mary's (CA)",
+        seedNumber: 5,
+        finalScore: 63,
+         
+    },
+    
+    {
+        teamName: "VCU",
+        seedNumber: 12,
+        finalScore: 51,
+        
+    },
+    {
+        teamName: "UConn",
+        seedNumber: 4,
+        finalScore: 87,
+         
+    },
+    {
+        teamName: "Iona",
+        seedNumber: 13,
+        finalScore: 63,
+         
+    },
+    {
+        teamName: "TCU",
+        seedNumber: 6,
+        finalScore: 72,
+         
+    },
+    {
+        teamName: "Arizona St",
+        seedNumber: 11,
+        finalScore: 70,
+         
+    },
+    {
+        teamName: "Gonzaga",
+        seedNumber: 3,
+        finalScore: 82,
+        
+    },
+    {
+        teamName: "Grand Canyon",
+        seedNumber: 14,
+        finalScore: 70,
+         
+    },
+    {
+        teamName: "Northwestern",
+        seedNumber: 7,
+        finalScore: 75,
+         
+    },
+    {
+        teamName: "Boise St",
+        seedNumber: 10,
+        finalScore: 67,
+         
+    },
+    {
+        teamName: "UCLA",
+        seedNumber: 2,
+        finalScore: 86,
+         
+    },
+    {
+        teamName: "UNC Asheville",
+        seedNumber: 15,
+        finalScore: 53,
+    },    
+];
+        
+let ncaaEast = [
+    {
+        teamName: "Purdue",
+        seedNumber: 1,
+        finalScore: 58,
+         
+    },
+    {
+        teamName: "FDU",
+        seedNumber: 16,
+        finalScore: 63,
+         
+    },
+    {
+        teamName: "Memphis",
+        seedNumber: 8,
+        finalScore: 65,
+         
+    },
+    {   teamName: "Fla. Atlantic",         
+        seedNumber: 9,
+        finalScore: 66,
+         
+    },
+    {
+        teamName:"Duke",
+        seedNumber: 5,
+        finalScore: 74,
+         
+    },
+    {
+        teamName:"Oral Roberts",
+        seedNumber: 12,
+        finalScore: 51,
+         
+    },
+    {
+        teamName: "Tennesee",
+        seedNumber: 8,
+        finalScore: 58,
+    },
+    {
+        teamName: "Louisiana",
+        seedNumber: 13,
+        finalScore: 55,
+    },
+    {
+        teamName: "Kentucky",
+        seedNumber: 6,
+        finalScore: 61,
+    },
+    {
+        teamName:"Providence",
+        seedNumber: 11,
+        finalScore: 53,
+
+    },
+    {
+        teamName: "Kansas St.",
+        seedNumber: 3,
+        finalScore: 77,
+    
+    },
+    {
+        teamName: "Montana St.",
+        seedNumber: 14,
+        finalScore: 65,
+    
+    },
+    {
+        teamName: "Michigan St.",
+        seedNumber: 7,
+        finalScore: 72,
+    
+    },
+    {
+        teamName: "Southern California",
+        seedNumber:10,
+        finalScore: 62,
+        
+    },
+    {
+        teamName: "Marquette",
+        seedNumber: 2,
+        finalScore: 78,
+        
+    },
+    {
+        teamName:"Vermont",
+        seedNumber: 15,
+        finalScore: 61,
+        
+    },
+];
+
+function getBracket() {
+    getBracketSouth();
+    function getBracketSouth() {
+
+        for(i = 0; i < ncaaSouth.length; i += 2) {
+            let div = document.createElement('div')
+            div.setAttribute('class', 'bracket1');
+    
+            let teamASouthTag = document.createElement('p')
+            teamASouthTag.innerText = "Seed: " + ncaaSouth[i].seedNumber +  "\nName: " + ncaaSouth[i].teamName +  "\nScore: " + ncaaSouth[i].finalScore;
+            let teamBSouthTag = document.createElement('p')
+            teamBSouthTag.innerText = "Seed: " + ncaaSouth[i + 1].seedNumber + "\nName: " + ncaaSouth[i + 1].teamName + "\nScore: " + ncaaSouth[i + 1].finalScore;
+            
+            document.body.appendChild(div);
+            div.appendChild(teamASouthTag);
+            div.appendChild(teamBSouthTag);
+            
+        }
+    }
+
+    getBracketMidwest();
+    function getBracketMidwest() {
+
+        for(i = 0; i < ncaaMidwest.length; i += 2) {
+            let div = document.createElement('div')
+            div.setAttribute('class', 'bracket2');
+    
+            let teamAMidwestTag = document.createElement('p')
+            teamAMidwestTag.innerText = "Seed: " + ncaaMidwest[i].seedNumber +  "\nName: " + ncaaMidwest[i].teamName +  "\nScore: " + ncaaMidwest[i].finalScore;
+            let teamBMidwestTag = document.createElement('p')
+            teamBMidwestTag.innerText = "Seed: " + ncaaMidwest[i + 1].seedNumber + "\nName: " + ncaaMidwest[i + 1].teamName + "\nScore: " + ncaaMidwest[i + 1].finalScore;
+            
+            document.body.appendChild(div);
+            div.appendChild(teamAMidwestTag);
+            div.appendChild(teamBMidwestTag);
+            
+        }
+    }
+
+    getBracketEast();
+    function getBracketEast() {
+
+        for(i = 0; i < ncaaEast.length; i += 2) {
+            let div = document.createElement('div')
+            div.setAttribute('class', 'bracket3');
+    
+            let teamAEastTag = document.createElement('p')
+            teamAEastTag.innerText = "Seed: " + ncaaEast[i].seedNumber +  "\nName: " + ncaaEast[i].teamName +  "\nScore: " + ncaaEast[i].finalScore;
+            let teamBEastTag = document.createElement('p')
+            teamBEastTag.innerText = "Seed: " + ncaaEast[i + 1].seedNumber + "\nName: " + ncaaEast[i + 1].teamName + "\nScore: " + ncaaEast[i + 1].finalScore;
+            
+            document.body.appendChild(div);
+            div.appendChild(teamAEastTag);
+            div.appendChild(teamBEastTag);
+            
+        }
+    }
+
+    getBracketWest();
+    function getBracketWest() {
+
+        for(i = 0; i < ncaaWest.length; i += 2) {
+            let div = document.createElement('div')
+            div.setAttribute('class', 'bracket4');
+    
+            let teamAWestTag = document.createElement('p')
+            teamAWestTag.innerText = "Seed: " + ncaaWest[i].seedNumber +  "\nName: " + ncaaWest[i].teamName +  "\nScore: " + ncaaWest[i].finalScore;
+            let teamBWestTag = document.createElement('p')
+            teamBWestTag.innerText = "Seed: " + ncaaWest[i + 1].seedNumber + "\nName: " + ncaaWest[i + 1].teamName + "\nScore: " + ncaaWest[i + 1].finalScore;
+            
+            document.body.appendChild(div);
+            div.appendChild(teamAWestTag);
+            div.appendChild(teamBWestTag);
+            
+        }
+    }
+
+}
+
+
+
