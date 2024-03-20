8179417e90b0eae907f5547ee55f56698ea4d4cb
COR Coding <corcoding@Jeremys-iPhone.localdomain>
2024-03-20 16:09:47 -0400

    logo url added

---
+++ ncaa.js
@@ -0,0 +1,450 @@
+
+
+
+
+// NCAA API 
+
+let ncaaSouth = [
+    {
+        teamName: "Alabama",
+        seedNumber: 1,
+        finalScore: 96,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/alabama.svg",
+    },
+    {
+        teamName: "Texas A&M-Corpus Christi",
+        seedNumber: 16,
+        finalScore: 75,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/am-corpus-chris.svg",
+    },
+    {
+        teamName: "Maryland",
+        seedNumber: 8,
+        finalScore: 67,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/maryland.svg",
+    },
+    {
+        teamName: "West Virgina",
+        seedNumber: 8,
+        finalScore: 65,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/west-virginia.svg",
+    },
+    {
+        teamName: "San Diego St.",
+        seedNumber: 5,
+        finalScore: 63,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/san-diego-st.svg",
+    },
+    {
+        teamName: "Col. of Charleston",
+        seedNumber: 12,
+        finalScore: 57,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/col-of-charleston.svg",
+    },
+    {
+        teamName: "Virginia",
+        seedNumber: 4,
+        finalScore: 67,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/virginia.svg",
+    },
+    {
+        teamName: "Furman",
+        seedNumber: 13,
+        finalScore: 68,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/furman.svg",
+    },
+    {
+        teamName: "Creighton",
+        seedNumber: 6,
+        finalScore: 72,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/creighton.svg",
+    },
+    {
+        teamName: "NC State",
+        seedNumber: 11,
+        finalScore: 63,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/north-carolina-st.svg",
+    },
+    {
+        teamName: "Baylor",
+        seedNumber: 3,
+        finalScore: 74,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/baylor.svg", 
+    },
+    {
+        teamName: "UC Santa Barbara",
+        seedNumber: 14,
+        finalScore: 56,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/uc-santa-barbara.svg", 
+    },
+    {
+        teamName: "Missouri",
+        seedNumber: 7,
+        finalScore: 76,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/missouri.svg",
+    },
+    {
+        teamName: "Utah St.",
+        seedNumber: 10,
+        finalScore: 65,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/utah-st.svg",
+    },
+    {
+        teamName: "Arizona",
+        seedNumber: 2,
+        finalScore: 55,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/arizona.svg",
+    },
+    {
+        teamName: "Princeton",
+        seedNumber: 15,
+        finalScore: 59,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/princeton.svg",
+    },
+
+];
+
+let ncaaMidwest = [
+    {
+        teamName: "Houston",
+        seedNumber: 1,
+        finalScore: 63,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/houston.svg",
+    },
+    
+    {
+        teamName: "Northern Kentucky",
+        seedNumber: 16,
+        finalScore: 52,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/northern-ky.svg",
+    },
+    
+    {
+        teamName: "Iowa",
+        seedNumber: 8,
+        finalScore: 75,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/iowa.svg",
+    },
+    {
+        teamName: "Auburn",
+        seedNumber: 9,
+        finalScore: 83,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/auburn.svg",
+    },
+    
+    {
+        teamName: "Miami",
+        seedNumber: 5,
+        finalScore: 63,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/miami-fl.svg",
+    },
+    
+    {
+        teamName: "Drake",
+        seedNumber: 12,
+        finalScore: 56,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/drake.svg",
+    },
+    
+    {
+        teamName: "Indiana",
+        seedNumber: 4,
+        finalScore: 71,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/indiana.svg",
+    },
+    {
+        teamName: "Kent St.",
+        seedNumber: 13,
+        finalScore: 60,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/kent-st.svg",
+    },
+    {
+        teamName: "Iowa St",
+        seedNumber: 6,
+        finalScore: 52,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/iowa-st.svg",
+    },
+    
+    {
+    teamName: "Pittsburgh",
+        seedNumber: 11,
+        finalScore: 59,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/pittsburgh.svg",
+    },
+    {
+    teamName: "Xavier",
+        seedNumber: 3,
+        finalScore: 72,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/xavier.svg",
+    },
+    {
+    
+     teamName: "Kennesaw St",
+        seedNumber: 14,
+        finalScore: 67,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/kennesaw-st.svg",
+        },
+
+     {
+        teamName: "Texas A&M",
+         seedNumber: 7,
+        finalScore: 59,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/texas-am.svg",
+
+     },
+     {
+        teamName: "Penn St",
+            seedNumber: 10,
+            finalScore: 76,
+            icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/penn-st.svg",
+        },
+        
+     {
+            teamName: "Texas",
+                seedNumber: 2,
+                finalScore: 81,
+                icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/texas.svg",
+            },
+              
+     {
+         teamName: "Colgate",
+             seedNumber: 15,
+             finalScore: 61,
+             icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/colgate.svg",
+                },
+                
+
+    
+];
+
+let ncaaWest = [
+    {
+        teamName: "Kansas",
+        seedNumber: 1,
+        finalScore: 96,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/kansas.svg",
+    },
+    
+    {
+       teamName: "Howard",
+       seedNumber: 16,
+       finalScore: 68,
+       icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/howard.svg",
+    },
+    {
+        teamName:"Arkansas",
+        seedNumber: 8,
+        finalScore: 73,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/arkansas.svg",
+    },
+    {
+        teamName: "Illinois",
+        seedNumber: 9,
+        finalScore: 63,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/illinois.svg",
+    },
+    {
+        teamName:"Saint Mary's (CA)",
+        seedNumber: 5,
+        finalScore: 63,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/st-marys-ca.svg",
+    },
+    
+    {
+        teamName: "VCU",
+        seedNumber: 12,
+        finalScore: 51,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/vcu.svg",
+    },
+    {
+        teamName: "UConn",
+        seedNumber: 4,
+        finalScore: 87,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/uconn.svg",
+    },
+    {
+        teamName: "Iona",
+        seedNumber: 13,
+        finalScore: 63,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/iona.svg",
+    },
+    {
+        teamName: "TCU",
+        seedNumber: 6,
+        finalScore: 72,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/tcu.svg",
+    },
+    {
+        teamName: "Arizona St",
+        seedNumber: 11,
+        finalScore: 70,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/arizona-st.svg",
+    },
+    {
+        teamName: "Gonzaga",
+        seedNumber: 3,
+        finalScore: 82,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/gonzaga.svg",
+    },
+    {
+        teamName: "Grand Canyon",
+        seedNumber: 14,
+        finalScore: 70,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/grand-canyon.svg",
+    },
+    {
+        teamName: "Northwestern",
+        seedNumber: 7,
+        finalScore: 75,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/northwestern.svg",
+    },
+    {
+        teamName: "Boise St",
+        seedNumber: 10,
+        finalScore: 67,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/boise-st.svg",
+    },
+    {
+        teamName: "UCLA",
+        seedNumber: 2,
+        finalScore: 86,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/ucla.svg",
+    },
+    {
+        teamName: "UNC Asheville",
+        seedNumber: 15,
+        finalScore: 53,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/unc-asheville.svg",
+        
+    },    
+]   
+        let ncaaEast = [
+    {
+        teamName: "Purdue",
+        seedNumber: 1,
+        finalScore: 58,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/purdue.svg",
+    },
+    {
+        teamName: "FDU",
+        seedNumber: 16,
+        finalScore: 63,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/fairleigh-dickinson.svg",
+    },
+    {
+        teamName: "Memphis",
+        seedNumber: 8,
+        finalScore: 65,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/memphis.svg",
+    },
+    {   teamName: "Fla. Atlantic",         
+        seedNumber: 9,
+        finalScore: 66,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/fla-atlantic.svg",
+    },
+    {
+        teamName:"Duke",
+        seedNumber: 5,
+        finalScore: 74,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/duke.svg",
+    },
+    {
+        teamName:"Oral Roberts",
+        seedNumber: 12,
+        finalScore: 51,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/oral-roberts.svg",
+    },
+    {
+        teamName: "Tennesee",
+        seedNumber: 8,
+        finalScore: 58,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/tennessee.svg",
+    },
+    {
+        teamName: "Louisiana",
+        seedNumber: 13,
+        finalScore: 55,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/la-lafayette.svg",
+    },
+    {
+        teamName: "Kentucky",
+        seedNumber: 6,
+        finalScore: 61,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/kentucky.svg",
+    },
+    {
+        teamName:"Providence",
+        seedNumber: 11,
+        finalScore: 53,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/providence.svg",
+    },
+    {
+        teamName: "Kansas St.",
+        seedNumber: 3,
+        finalScore: 77,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/kansas-st.svg",
+    },
+    {
+        teamName: "Montana St.",
+        seedNumber: 14,
+        finalScore: 65,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/montana-st.svg",
+    },
+    {
+        teamName: "Michigan St.",
+        seedNumber: 7,
+        finalScore: 72,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/michigan-st.svg",
+    },
+    {
+        teamName: "Southern California",
+        seedNumber:10,
+        finalScore: 62,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/southern-california.svg",
+    },
+    {
+        teamName: "Marquette",
+        seedNumber: 2,
+        finalScore: 78,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/marquette.svg",
+    },
+    {
+        teamName:"Vermont",
+        seedNumber: 15,
+        finalScore: 61,
+        icon: "https://i.turner.ncaa.com/sites/default/files/images/logos/schools/bgl/vermont.svg",
+    },
+]
+
+function getBracket() {
+    for(i = 0; i < 8; i++) {
+        let teamASouth = ncaaSouth[0].seedNumber;
+        console.log(teamASouth);
+        let teamBSouth = ncaaSouth[0].seedNumber;
+        console.log(teamBSouth);
+    }
+    
+}
+function getBracket() {
+
+    for(i = 0; i < ncaaSouth.length; i += 2) {
+        let div = document.createElement('div')
+        div.setAttribute('class', 'bracket1');
+
+        let teamASouthTag = document.createElement('p')
+        teamASouthTag.innerText = ncaaSouth[i].seedNumber + " " + ncaaSouth[i].teamName + " " + ncaaSouth[i].finalScore;
+        let teamBSouthTag = document.createElement('p')
+        teamBSouthTag.innerText = ncaaSouth[i + 1].seedNumber + " " + ncaaSouth[i + 1].teamName + " " + ncaaSouth[i + 1].finalScore;
+        
+        document.body.appendChild(div)
+        div.appendChild(teamASouthTag);
+        div.appendChild(teamBSouthTag);
+        
+    }
+
+}
+
+
+
+getBracket();
+
